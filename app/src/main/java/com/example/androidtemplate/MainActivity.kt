069c7eb4b58b7ebf2e855dc18d622f3eb8e5ef24
Dan-Arfi <danarfi07@gmail.com>
2023-10-21 12:28:55 +0300

    asd

---
+++ app/src/main/java/com/example/androidtemplate/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.app1697879824973
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.example.app1697879824973.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var urlWebView: WebView
+    private var url: String = "https://dev.to/tqbit/quick-dirty-how-to-deploy-a-fullstack-vue-js-app-with-a-working-node-js-backend-51k4" // Default URL
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main)
+
+        urlWebView = findViewById(R.id.urlWebView)
+        val webSettings: WebSettings = urlWebView.settings
+        webSettings.javaScriptEnabled = true // Enable JavaScript
+        webSettings.domStorageEnabled = true // Enable DOM Storage
+        webSettings.allowFileAccess = true // Allow access to file
+        webSettings.allowContentAccess = true // Allow content access
+
+        urlWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                return when {
+                    url.startsWith("tel:") -> {
+                        val intent = Intent(Intent.ACTION_DIAL)
+                        intent.data = Uri.parse(url)
+                        startActivity(intent)
+                        true
+                    }
+                    url.startsWith("mailto:") -> {
+                        val intent = Intent(Intent.ACTION_SENDTO)
+                        intent.data = Uri.parse(url)
+                        startActivity(intent)
+                        true
+                    }
+                    url.startsWith("sms:") -> {
+                        val intent = Intent(Intent.ACTION_SENDTO)
+                        intent.data = Uri.parse(url)
+                        startActivity(intent)
+                        true
+                    }
+                    url.startsWith("http:") || url.startsWith("https:") -> {
+                        view.loadUrl(url)
+                        false
+                    }
+                    else -> {
+                        try {
+                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                            startActivity(intent)
+                            true
+                        } catch (e: Exception) {
+                            false
+                        }
+                    }
+                }
+            }
+        }
+        urlWebView.loadUrl(url)
+    }
+
+    override fun onBackPressed() {
+        if (urlWebView.canGoBack()) {
+            urlWebView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
+

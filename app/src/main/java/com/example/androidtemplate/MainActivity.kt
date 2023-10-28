c420d9e65cc89287c014e06086b7f8edca902213
Dan-Arfi <danarfi07@gmail.com>
2023-10-28 21:44:56 +0300

    asda

--- app/src/main/java/com/example/androidtemplate/MainActivity.kt
+++ app/src/main/java/com/example/androidtemplate/MainActivity.kt
@@ -1,95 +1,100 @@
-package com.example.androidTemplate;
+package com.example.androidTemplate
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-import android.webkit.*;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
-import com.example.androidTemplate.R;
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.webkit.*
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidTemplate.R
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity() {
 
-    private WebView urlWebView;
-    private LinearLayout noInternetLayout;
-    private String url = "https://dev.to/tqbit/quick-dirty-how-to-deploy-a-fullstack-vue-js-app-with-a-working-node-js-backend-51k4"; // Default URL
+    private lateinit var urlWebView: WebView
+    private lateinit var noInternetLayout: LinearLayout
+    private var url: String = "https://dev.to/tqbit/quick-dirty-how-to-deploy-a-fullstack-vue-js-app-with-a-working-node-js-backend-51k4" // Default URL
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
-        setContentView(R.layout.activity_main);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_main)
 
-        urlWebView = findViewById(R.id.urlWebView);
-        noInternetLayout = findViewById(R.id.noInternetLayout);
-        WebSettings webSettings = urlWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true); // Enable JavaScript
-        webSettings.setDomStorageEnabled(true); // Enable DOM Storage
-        webSettings.setAllowFileAccess(true); // Allow access to file
-        webSettings.setAllowContentAccess(true); // Allow content access
+        urlWebView = findViewById(R.id.urlWebView)
+        noInternetLayout = findViewById(R.id.noInternetLayout)
+        val webSettings: WebSettings = urlWebView.settings
+        webSettings.javaScriptEnabled = true // Enable JavaScript
+        webSettings.domStorageEnabled = true // Enable DOM Storage
+        webSettings.allowFileAccess = true // Allow access to file
+        webSettings.allowContentAccess = true // Allow content acces
 
-        // Set custom User-Agent
-        String USER_AGENT = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
-        webSettings.setUserAgentString(USER_AGENT);
 
-        urlWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onReceivedError(
-                    WebView view,
-                    WebResourceRequest request,
-                    WebResourceError error) {
+        urlWebView.webViewClient = object : WebViewClient() {
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
                 // Display custom no internet connection layout
-                urlWebView.setVisibility(WebView.GONE);
-                noInternetLayout.setVisibility(LinearLayout.VISIBLE);
+                urlWebView.visibility = WebView.GONE
+                noInternetLayout.visibility = LinearLayout.VISIBLE
             }
 
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (url.startsWith("tel:")) {
-                    Intent intent = new Intent(Intent.ACTION_DIAL);
-                    intent.setData(Uri.parse(url));
-                    startActivity(intent);
-                    return true;
-                } else if (url.startsWith("mailto:")) {
-                    Intent intent = new Intent(Intent.ACTION_SENDTO);
-                    intent.setData(Uri.parse(url));
-                    startActivity(intent);
-                    return true;
-                } else if (url.startsWith("sms:")) {
-                    Intent intent = new Intent(Intent.ACTION_SENDTO);
-                    intent.setData(Uri.parse(url));
-                    startActivity(intent);
-                    return true;
-                } else if (url.startsWith("http:") || url.startsWith("https:")) {
-                    view.loadUrl(url);
-                    return false;
-                } else {
-                    try {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                        startActivity(intent);
-                        return true;
-                    } catch (Exception e) {
-                        return false;
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                return when {
+                    url.startsWith("tel:") -> {
+                        val intent = Intent(Intent.ACTION_DIAL)
+                        intent.data = Uri.parse(url)
+                        startActivity(intent)
+                        true
+                    }
+                    url.startsWith("mailto:") -> {
+                        val intent = Intent(Intent.ACTION_SENDTO)
+                        intent.data = Uri.parse(url)
+                        startActivity(intent)
+                        true
+                    }
+                    url.startsWith("sms:") -> {
+                        val intent = Intent(Intent.ACTION_SENDTO)
+                        intent.data = Uri.parse(url)
+                        startActivity(intent)
+                        true
+                    }
+                    url.startsWith("http:") || url.startsWith("https:") -> {
+                        view.loadUrl(url)
+                        false
+                    }
+                    else -> {
+                        try {
+                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                            startActivity(intent)
+                            true
+                        } catch (e: Exception) {
+                            false
+                        }
                     }
                 }
             }
-        });
-        urlWebView.loadUrl(url);
+        }
+        urlWebView.loadUrl(url)
     }
 
-    @Override
-    public void onBackPressed() {
+    override fun onBackPressed() {
         if (urlWebView.canGoBack()) {
-            urlWebView.goBack();
+            urlWebView.goBack()
         } else {
-            super.onBackPressed();
+            super.onBackPressed()
         }
     }
 }
+
+
+
+
+
